21071b56e4964b0facc5623f704347e6f0d01c2a
Paolo Perego <thesp0nge@gmail.com>
2012-10-09 09:20:19 +0200

    Base CVSS vector fully parsed

--- spec/cvss_spec.rb
+++ spec/cvss_spec.rb
@@ -13,4 +13,30 @@ describe "CVSS library" do
   it "should recognize a good input" do
     cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C").should be_true
   end
+
+  it "should recognize Access Vector" do
+    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+    cvss.base[:av].should == "N"
+  end
+
+  it "should recognize Access Complexity" do
+    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+    cvss.base[:ac].should == "L"
+  end
+  it "should recognize Authentication" do
+    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+    cvss.base[:au].should == "N"
+  end
+  it "should recognize Confidentiality" do
+    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+    cvss.base[:c].should == "N"
+  end
+  it "should recognize Integrity" do
+    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+    cvss.base[:i].should == "N"
+  end
+  it "should recognize Availability" do
+    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+    cvss.base[:a].should == "C"
+  end
 end
