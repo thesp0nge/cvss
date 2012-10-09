4f8d304f1cd2f5c5de77bfda9646f293c03a151a
Paolo Perego <thesp0nge@gmail.com>
2012-10-09 09:38:56 +0200

    Added helpers

--- lib/cvss.rb
+++ lib/cvss.rb
@@ -1,9 +1,11 @@
 require "cvss/version"
 require 'cvss/parser'
+require 'cvss/helpers'
 
 module Cvss 
   class Engine
     include Cvss::Parser
+    include Cvss::Helpers
 
   end
 end
--- spec/cvss_spec.rb
+++ spec/cvss_spec.rb
@@ -2,41 +2,70 @@ require 'spec_helper'
 
 describe "CVSS library" do
   let(:cvss) { Cvss::Engine.new() } 
-  it "should have a parser method" do
-    cvss.should respond_to(:parse)
-    
-  end
-  it "should recognize a bad input" do
-    cvss.parse("this is a test string").should be_false
-  end
+  describe "parser" do
 
-  it "should recognize a good input" do
-    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C").should be_true
-  end
+    it "should have a parser method" do
+      cvss.should respond_to(:parse)
 
-  it "should recognize Access Vector" do
-    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
-    cvss.base[:av].should == "N"
-  end
+    end
+    it "should recognize a bad input" do
+      cvss.parse("this is a test string").should be_false
+    end
 
-  it "should recognize Access Complexity" do
-    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
-    cvss.base[:ac].should == "L"
+    it "should recognize a good input" do
+      cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C").should be_true
+    end
+
+    it "should recognize Access Vector" do
+      cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+      cvss.base[:av].should == "N"
+    end
+
+    it "should recognize Access Complexity" do
+      cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+      cvss.base[:ac].should == "L"
+    end
+    it "should recognize Authentication" do
+      cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+      cvss.base[:au].should == "N"
+    end
+    it "should recognize Confidentiality" do
+      cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+      cvss.base[:c].should == "N"
+    end
+    it "should recognize Integrity" do
+      cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+      cvss.base[:i].should == "N"
+    end
+    it "should recognize Availability" do
+      cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+      cvss.base[:a].should == "C"
+    end
   end
-  it "should recognize Authentication" do
-    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
-    cvss.base[:au].should == "N"
-  end
-  it "should recognize Confidentiality" do
-    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
-    cvss.base[:c].should == "N"
-  end
-  it "should recognize Integrity" do
-    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
-    cvss.base[:i].should == "N"
-  end
-  it "should recognize Availability" do
-    cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
-    cvss.base[:a].should == "C"
+  describe "helper" do
+    it "should have a data integrity helper" do
+      cvss.should respond_to(:data_integrity)
+    end
+    it "should have a data confidentiality helper" do
+      cvss.should respond_to(:data_confidentiality)
+    end
+    it "should have a data availability helper" do
+      cvss.should respond_to(:data_availability)
+    end
+
+    it "should recognize Confidentiality" do
+      cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+      cvss.data_confidentiality.should == "N"
+    end
+    it "should recognize Integrity" do
+      cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+      cvss.data_integrity.should == "N"
+    end
+    it "should recognize Availability" do
+      cvss.parse("AV:N/AC:L/Au:N/C:N/I:N/A:C")
+      cvss.data_availability.should == "C"
+    end
+
+
   end
 end
